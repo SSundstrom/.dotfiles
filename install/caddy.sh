09f3efaee13cc1f3efe301239a8fd17c809fcc24
Simon Sundström <ssundstrom@live.se>
2018-02-07 13:17:51 +0000

    updated to use hardlink for caddyfile

--- install/caddy.sh
+++ install/caddy.sh
@@ -6,7 +6,7 @@ curl https://getcaddy.com | bash
 # create folders and files for caddy
 sudo mkdir /etc/caddy
 sudo chown -R root:www-data /etc/caddy
-sudo ln -s $HOME/.dotfiles/settings/Caddyfile /etc/caddy/Caddyfile
+sudo ln $HOME/.dotfiles/settings/Caddyfile /etc/caddy/Caddyfile
 sudo mkdir /etc/ssl/caddy
 sudo chown -R www-data:root /etc/ssl/caddy
 sudo chmod 0770 /etc/ssl/caddy
@@ -14,7 +14,7 @@ sudo mkdir /var/www
 sudo chown www-data:www-data /var/www
 
 # firewall exceptions
-sudo ufw allow http 
+sudo ufw allow http
 sudo ufw allow https
 
 # autostart with systemctl
