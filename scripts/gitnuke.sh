1ca49ea20c70ee8d4d02defef3342e7ffb12cb05
Simon Sundström <ssundstrom@live.se>
2018-02-26 22:14:50 +0100

    made prompt for gitnuke prettier

--- scripts/gitnuke.sh
+++ scripts/gitnuke.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 BRANCH=$1
 
-git push -d origin $BRANCH
-git branch -d $BRANCH
+bold=`tput bold`
+normal=`tput sgr0`
+
+remove(){
+    git push -d origin $BRANCH
+    git branch -d $BRANCH
+}
+echo "Do you wish to delete branch ${bold}$BRANCH${normal} from remote and local?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) remove; break;;
+        No ) exit;;
+    esac
+done
+
