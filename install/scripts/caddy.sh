21d185816a8f659b0ab7855e2e371fc5b37c0459
Simon Sundström <ssundstrom@live.se>
2019-03-06 08:21:26 +0000

    update: removing files on setup if they are there

--- install/scripts/caddy.sh
+++ install/scripts/caddy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # create folders and files for caddy
+sudo rm -rf /etc/caddy
 sudo mkdir /etc/caddy
 sudo chown -R root:www-data /etc/caddy
+sudo rm /etc/caddy/Caddyfile
 sudo ln $HOME/.dotfiles/settings/Caddyfile /etc/caddy/Caddyfile
+sudo rm -rf /etc/ssl/caddy
 sudo mkdir /etc/ssl/caddy
 sudo chown -R www-data:root /etc/ssl/caddy
 sudo chmod 0770 /etc/ssl/caddy
@@ -15,6 +18,7 @@ sudo ufw allow http
 sudo ufw allow https
 
 # autostart with systemctl
+sudo rm /etc/systemd/system/caddy.service
 sudo ln -s $HOME/.dotfiles/settings/services/caddy.service /etc/systemd/system/caddy.service
 sudo systemctl daemon-reload
 sudo systemctl enable caddy.service
